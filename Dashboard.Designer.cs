7fa1730d59890eabd64ef781576639363ecf0666
dbryn24 <davebryangamer@gmail.com>
2024-11-04 14:15:36 +0800

    add searchbar

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -46,6 +46,7 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -163,7 +164,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Arial Black", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(885, 33);
+            this.label2.Location = new System.Drawing.Point(891, 33);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(355, 136);
@@ -252,19 +253,28 @@
             // 
             // label7
             // 
-            this.label7.Location = new System.Drawing.Point(893, 236);
+            this.label7.Location = new System.Drawing.Point(893, 297);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(163, 387);
+            this.label7.Size = new System.Drawing.Size(163, 326);
             this.label7.TabIndex = 18;
             this.label7.Text = "gridview";
             // 
             // label8
             // 
-            this.label8.Location = new System.Drawing.Point(1074, 236);
+            this.label8.Location = new System.Drawing.Point(1074, 297);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(166, 387);
+            this.label8.Size = new System.Drawing.Size(166, 326);
             this.label8.TabIndex = 19;
             this.label8.Text = "gridview";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(896, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 35);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.Text = "Search Member";
             // 
             // FormDashboard
             // 
@@ -272,6 +282,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -318,5 +329,6 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
